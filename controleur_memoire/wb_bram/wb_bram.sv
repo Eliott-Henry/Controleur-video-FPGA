2f78ba1d11b6107eb90ec67a5ceb24bcd40ccafa
Eliott Henry <eliott.henry@telecom-paris.fr>
2022-12-12 16:41:59 +0100

    debut tp wishbone

--- controleur_memoire/wb_bram/wb_bram.sv
+++ controleur_memoire/wb_bram/wb_bram.sv
@@ -11,6 +11,25 @@ module wb_bram #(parameter mem_adr_width = 11) (
       );
       // a vous de jouer a partir d'ici
 
+assign wb_s.err = 0;
+assign wb_s.rty = 0;
+// il y a d'autres assign qu'on peut faire (voir photo tableau)
+// quel octet modifier ? sel ou deux bits poids faible de l'adresse
+// mot = 4 octets [ | | | ] 
+// et sel sur 4b  [ | | | ] donc on sait quels octets prendre
 
-      endmodule
+always_ff @(posedge wb_s.clk) 
+begin
+      
+end
 
+endmodule
+
+/*
+
+Pour une ecriture : ACK au même cycle pour une écriture (combinatoirement) ack = strb
+Pour une lecture : quand STB, on reçoit une adresse, on produit une donnée mais elle ne sort qu'au cycle suivant (ack) (séquentiellement) (ack = strb en retard de 1 cycle)
+On va voir ack en cas de read et write différents
+
+Essayer de séparer les deux (mémoire et contrôle : ctrl génère juste ack au bon moment)
+*/
